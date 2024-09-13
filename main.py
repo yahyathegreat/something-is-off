3aeeeab2630d6fdc16b8eeca7edc58e178c4fb85
yahyathegreat <yahyamardan1234@gmail.com>
2024-09-13 20:15:19 +0500

    main.py

--- main.py
+++ main.py
@@ -14,4 +14,14 @@ if (saleamount > actualcost):
     print("total profit = {0}". format(amount))
 else:
     print("no profit!!!")
+
+
+i = int (input("enter a number : "))
+if (i < 15):
+    print(" i is smaller  than 15")
+    print("i'm in if block")
+else:
+    print (" i is greater than 15")
+    print (" i'm in else block")
+print ("i'm not in if and not in else block")
     
