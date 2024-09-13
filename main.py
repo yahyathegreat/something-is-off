c9928c9e797255db8022ed9c8a0259700c75b215
yahyathegreat <yahyamardan1234@gmail.com>
2024-09-13 20:04:32 +0500

    main.py

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ if num > 0:
 num = -1
 if num > 0:
     print(num, "is a positve number")
+
+
+
+actualcost = float(input("please enter actual product price:"))
+saleamount = float (input("please enter sales amount: "))
+if (saleamount > actualcost):
+    amount = saleamount - actualcost
+    print("total profit = {0}". format(amount))
+else:
+    print("no profit!!!")
+    
